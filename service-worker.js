08a60f616ebb7f9b26fea63b84c9a56414185ce1
狄世豪 <156295976+dishihao@users.noreply.github.com>
2026-07-20 12:55:24 +0800

    Enable cloud sync injection and refresh PWA cache

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
-const C='specimen-v5';const A=["./","./index.html","./data-01.js","./data-02.js","./data-03.js","./data-04.js","./data-05.js","./data-06.js","./data-07.js","./data-08.js","./data-09.js","./data-10.js","./manifest.webmanifest","./icon.svg"];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(x=>x.addAll(A))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const C='specimen-v7-cloud';
+const A=['./','./index.html','./cloud-sync.js','./data-01.js','./data-02.js','./data-03.js','./data-04.js','./data-05.js','./data-06.js','./data-07.js','./data-08.js','./data-09.js','./data-10.js','./manifest.webmanifest','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(x=>x.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+async function injectedIndex(){
+  let r;
+  try{r=await fetch('./index.html',{cache:'no-store'})}catch(_){r=await caches.match('./index.html')}
+  let t=await r.text();
+  if(!t.includes('cloud-sync.js'))t=t.replace('</body>','<script src="./cloud-sync.js"></script></body>');
+  return new Response(t,{headers:{'Content-Type':'text/html; charset=utf-8','Cache-Control':'no-store'}});
+}
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate')return e.respondWith(injectedIndex());
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
